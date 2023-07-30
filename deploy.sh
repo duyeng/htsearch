a0d4fc6443125941b074c27d286f365938cc3e87
duyeng <140710227+duyeng@users.noreply.github.com>
2023-07-30 11:35:48 +0700

    Deploy (#10)

--- deploy.sh
+++ deploy.sh
@@ -9,3 +9,23 @@ if [[ "$env" == "dev" ]]; then
     docker-compose run api rails db:setup
     docker-compose up
 fi
+
+if [[ "$env" == "prod" ]]; then
+    aws eks update-kubeconfig --name ${AWS_EKS_CLUSTER_NAME} --region ${AWS_REGION}
+
+#    kubectl create secret docker-registry regcred \
+#      --docker-server=${AWS_ACCOUNT}.dkr.ecr.${AWS_REGION}.amazonaws.com \
+#      --docker-username=AWS \
+#      --docker-password=$(aws ecr get-login-password)
+
+    echo "Deploy mysql"
+    kubectl apply -f ./k8s/pv.yaml
+    kustomize build ./k8s/mysql | kubectl apply -f -
+    echo "Deploy redis"
+    kustomize build ./k8s/redis | kubectl apply -f -
+    echo "Deploy api"
+    kustomize build ./k8s/api | kubectl apply -f -
+    echo "Deploy worker"
+    kustomize build ./k8s/worker | kubectl apply -f -
+fi
+
